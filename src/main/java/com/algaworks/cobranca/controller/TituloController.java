39109f5db5928c8f40cd87455398a44df08726c8
Christian Mulato <chmulato@hotmail.com>
2017-01-15 09:06:45 -0200

    projeto cobranca spring boot

---
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -0,0 +1,89 @@
+package com.algaworks.cobranca.controller;
+
+	
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.cobranca.model.StatusTitulo;
+import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.repository.filter.TituloFilter;
+import com.algaworks.cobranca.service.TituloService;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
+	@Autowired
+	private TituloService tituloService;
+	
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		mv.addObject(new Titulo());
+		return mv;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
+		try {
+			tituloService.salvar(titulo);
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch (IllegalArgumentException ex) {
+			errors.rejectValue("dataVencimento", null, ex.getMessage());
+			return CADASTRO_VIEW;
+		}
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar(@ModelAttribute("filtro") TituloFilter filtro) {
+		List<Titulo> todosTitulos = tituloService.filtrar(filtro);
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable Long codigo) {
+		Titulo titulo = tituloService.buscar(codigo);
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		tituloService.excluir(codigo);
+		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
+	}
+	
+	
+	@RequestMapping(value= "/{codigo}/receber", method = RequestMethod.PUT)
+	public @ResponseBody String receber(@PathVariable Long codigo) {
+		return tituloService.receber(codigo);
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
+	}
+}
